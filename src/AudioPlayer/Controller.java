b49fca221bdd9980b56aef4c160bce6df52919e7
munna19 <abdulalmamun971@yahoo.com>
2021-04-29 00:35:05 +0600

    audioPlayer_javafx

---
+++ src/AudioPlayer/Controller.java
@@ -0,0 +1,13 @@
+package AudioPlayer;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
---
+++ src/AudioPlayer/Main.java
@@ -0,0 +1,23 @@
+package AudioPlayer;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("designAP.fxml"));
+        primaryStage.setTitle("AudioPlayer");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
