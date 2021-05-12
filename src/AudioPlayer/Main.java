71f527c33919952a9d79c0b9b00f2be1c6dc41de
munna19 <abdulalmamun971@yahoo.com>
2021-05-13 00:08:05 +0600

    AudioPlayer

--- src/AudioPlayer/Main.java
+++ src/AudioPlayer/Main.java
@@ -19,7 +19,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("designAP.fxml"));
         primaryStage.setTitle("AudioPlayer");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 400, 475));
         primaryStage.show();
     }
 
