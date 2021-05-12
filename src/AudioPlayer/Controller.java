3f87e9071dd966e62250dc17c4e1afaa27e042e7
munna19 <abdulalmamun971@yahoo.com>
2021-05-12 23:59:23 +0600

    AudioPlayer

--- src/AudioPlayer/Controller.java
+++ src/AudioPlayer/Controller.java
@@ -4,7 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
+import java.io.FileInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -44,5 +47,13 @@ public class Controller implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        try {
+            play_button_id.setGraphic(new ImageView(new Image(new FileInputStream("src/icons/play.png"))));
+            previous_button_id.setGraphic(new ImageView(new Image(new FileInputStream("src/icons/previous.png"))));
+            next_button_id.setGraphic(new ImageView(new Image(new FileInputStream("src/icons/next.png"))));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
