27e0101179512b63126503400c9a43f58cc632fb
munna19 <abdulalmamun971@yahoo.com>
2021-05-19 23:05:39 +0600

    AudioPlayer

--- src/AudioPlayer/Controller.java
+++ src/AudioPlayer/Controller.java
@@ -1,17 +1,29 @@
 package AudioPlayer;
 
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.stage.FileChooser;
+import javafx.util.Duration;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 /*
@@ -22,11 +34,24 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
+    private int[] speeds ={25,50,75,100,125,150,175,200};
+    private Media media;
+    private MediaPlayer mediaPlayer;
+    private File directory;
+    private File[] files;
+    private ArrayList<File> songs;
+    private int songNumber;
+
+
+    @FXML
+    private Label label_id;
+
     @FXML
     private Slider timer_slider_id;
 
     @FXML
-    private ComboBox<?> speed_box_id;
+    private ComboBox<String> speed_box_id;
+    //private ComboBox<?> speed_box_id;
 
     @FXML
     private Button previous_button_id;
@@ -39,13 +64,70 @@ public class Controller implements Initializable {
 
     @FXML
     void Add_music_Action(ActionEvent event) {
+        try {
+            FileChooser fc = new FileChooser();
+            File selectedFile = fc.showOpenDialog(null);
+            media = new Media(selectedFile.toURI().toString());
 
+            mediaPlayer = new MediaPlayer(media);
+
+
+            //all operation relies
+            /*if(mediaPlayer != null){
+                mediaPlayer.dispose();
+            }*/
+
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        //time slider
+        mediaPlayer.setOnReady(()->{
+            //this function work when the player gets ready
+            timer_slider_id.setMin(0);
+            timer_slider_id.setMax(mediaPlayer.getMedia().getDuration().toMinutes());
+            timer_slider_id.setValue(0);
+
+            try {
+                play_button_id.setGraphic(new ImageView(new Image(new FileInputStream("src/icons/play.png"))));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+        //listener on player
+        mediaPlayer.currentTimeProperty().addListener(new ChangeListener<Duration>(){
+            @Override
+            public void changed(ObservableValue<? extends Duration> observable, Duration oldValue, Duration newValue) {
+
+                Duration durationD = mediaPlayer.getCurrentTime();
+
+                //slider_id.setValue(durationD.toSeconds());
+                timer_slider_id.setValue(durationD.toMinutes());
+
+            }
+
+        });
+
+        // slider position change
+        timer_slider_id.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
+                if(timer_slider_id.isPressed()){
+                    double slider_value = timer_slider_id.getValue();
+                    //player.seek(new Duration(slider_value*1000));
+                    mediaPlayer.seek(new Duration(slider_value * 60 * 1000)); // for minute
+                }
+            }
+        });
     }
 
 
     @FXML
     void PlayButton_Action(ActionEvent event) {
-
+        mediaPlayer.play();
     }
 
     @FXML
@@ -79,5 +161,7 @@ public class Controller implements Initializable {
         catch (Exception e){
             e.printStackTrace();
         }
+
+        songs = new ArrayList<File>();
     }
 }
